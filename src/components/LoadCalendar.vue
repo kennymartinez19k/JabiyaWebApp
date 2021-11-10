d77ffdf938c911c2a03a9156855529f2d66fbc0c
wilsonSoto <77066800+wilsonSoto@users.noreply.github.com>
2021-11-10 15:00:01 -0400

    load configuration for api connection

--- src/components/LoadCalendar.vue
+++ src/components/LoadCalendar.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
     <BackButton/>
-       <div class="uk-background-muted  uk-panel uk-flex uk-flex-around">
+       <div class="uk-background-muted  uk-panel uk-flex uk-flex-around sticky">
               <div class="uk-padding-small">
             <p class="uk-text-middle">Tiempo</p>
            </div>
             <div class="uk-padding-small">
             <p class="uk-text-middle">Estatus</p>
             </div>
-        </div>
+      </div>
+       <div v-for="load in loads" :key="load" class="uk-flex uk-flex-around">
+           <div class="uk-padding-small" style="width:40%">
+             <span> {{load.hours}} </span>
+            </div>
+             <div class="uk-padding-small">
+             <font-awesome-icon icon="truck" :class="{iconfaDeliverd:load.status === 'Delivered', iconfaDispatchedApproved:load.status === 'Dispatched Approved', iconfaDispatched:load.status === 'Dispatched', iconfaAssignedToDriver:load.status === 'Assigned to Driver'}"/>
+            </div>
+             <div class="uk-padding-small uk-text-right" style="width:40%">
+               <span> {{load.status}} </span>
+            </div>
+      </div>
   </div>
 </template>
 
@@ -18,6 +29,43 @@ import BackButton from '../components/Buttons/BackButton.vue'
 export default {
   components: {
     BackButton,
+  },
+  data () {
+    return {
+      loads: [
+       { hours: '10:00 - 10:30', status: 'Delivered'},
+       { hours: '12:00 - 01:30', status: 'Dispatched Approved'},
+       { hours: '03:00 - 03:30', status: 'Dispatched'},
+       { hours: '04:00 - 04:30', status: 'Assigned to Driver'},
+       { hours: '05:00 - 05:30', status: 'Delivered'},
+       { hours: '10:00 - 10:30', status: 'Delivered'},
+       { hours: '12:00 - 01:30', status: 'Dispatched Approved'},
+       { hours: '03:00 - 03:30', status: 'Dispatched'},
+       { hours: '04:00 - 04:30', status: 'Assigned to Driver'},
+       { hours: '05:00 - 05:30', status: 'Delivered'},
+       { hours: '10:00 - 10:30', status: 'Delivered'},
+       { hours: '12:00 - 01:30', status: 'Dispatched Approved'},
+       { hours: '03:00 - 03:30', status: 'Dispatched'},
+       { hours: '04:00 - 04:30', status: 'Assigned to Driver'},
+       { hours: '05:00 - 05:30', status: 'Delivered'},
+       { hours: '10:00 - 10:30', status: 'Delivered'},
+       { hours: '12:00 - 01:30', status: 'Dispatched Approved'},
+       { hours: '03:00 - 03:30', status: 'Dispatched'},
+       { hours: '04:00 - 04:30', status: 'Assigned to Driver'},
+       { hours: '05:00 - 05:30', status: 'Delivered'},
+       { hours: '10:00 - 10:30', status: 'Delivered'},
+       { hours: '12:00 - 01:30', status: 'Dispatched Approved'},
+       { hours: '03:00 - 03:30', status: 'Dispatched'},
+       { hours: '04:00 - 04:30', status: 'Assigned to Driver'},
+       { hours: '05:00 - 05:30', status: 'Delivered'},
+       { hours: '10:00 - 10:30', status: 'Delivered'},
+       { hours: '12:00 - 01:30', status: 'Dispatched Approved'},
+       { hours: '03:00 - 03:30', status: 'Dispatched'},
+       { hours: '04:00 - 04:30', status: 'Assigned to Driver'},
+       { hours: '05:00 - 05:30', status: 'Delivered'},
+
+      ]
+    }
   }
 
 }
@@ -26,5 +74,28 @@ export default {
 <style scoped>
 p {
     margin: 0px;
+}
+.iconfaDeliverd {
+   font-size: 20px;
+   color: rgb(13, 99, 10);
+}
+.iconfaDispatchedApproved {
+   font-size: 20px;
+   color: rgb(5, 7, 5);
+}
+.iconfaDispatched {
+   font-size: 20px;
+   color: rgb(255, 174, 0);
+}
+.iconfaAssignedToDriver {
+   font-size: 20px;
+   color: rgb(203, 207, 203);
+}
+.sticky {
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  padding: 5px;
+  border: 2px solid #564caf;
 }
 </style>>
--- src/main.js
+++ src/main.js
@@ -7,12 +7,14 @@ import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core' 
-import { fas, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { fas, faArrowLeft, faTruck, faTruckPickup, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { faPhone } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 library.add(fas, faYoutube)
-
+library.add(faUser)
+library.add(faTruck)
+library.add(faTruckPickup)
 library.add(faPhone)
 library.add(faArrowLeft)
 library.add(faTwitter)
